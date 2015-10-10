87cc7a42d0fc48166e296a51b019ee333a507d8d
Vlod Kalicun <vlod@vlod.com>
2015-10-10 05:32:15 -0600

    2

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,16 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 
-module.exports = {
+var common = {
   entry: path.resolve(ROOT_PATH, 'app'),
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
-  },
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true
   },
   module: {
     loaders: [
@@ -28,9 +23,24 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: 'Kanban app'
     })
   ]
 };
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
